a72cd850d0678d2133b11541d37c677da8103f8e
E.Shangeraev <e.shangeraev@krastsvetmet.ru>
2023-09-17 15:46:14 +0600

    build(vite): configuring vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, ConfigEnv, UserConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const baseConfig: UserConfig = {
+    plugins: [react()],
+  };
+
+  const config: Record<ConfigEnv['command'], UserConfig> = {
+    serve: {
+      ...baseConfig,
+      server: {
+        port: 3000,
+        open: true,
+      },
+      build: {
+        sourcemap: 'inline',
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './src'),
+          '@assets': path.resolve(__dirname, './src/assets'),
+        },
+      },
+    },
+
+    build: {
+      ...baseConfig,
+    },
+  };
+
+  return config[command];
+});
